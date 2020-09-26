7c07c04fc1ead97326a55c5ae0d38c1a48a06e70
Giovanni d'Ario <giovanni.dario@roche.com>
2020-09-26 18:52:04 +0200

    Remove buggy custom log-loss.

--- src/utils.py
+++ src/utils.py
@@ -13,17 +13,3 @@ def fill_results(idx_cp, results):
     out = np.zeros((idx_cp.shape[0], results.shape[1]))
     out[idx_cp == 1] = results
     return out
-
-
-def stabilize_y_hat(y_hat):
-    y_high = np.zeros_like(y_hat)
-    y_high.fill(1.0-1.0e-15)
-    y_low = np.zeros_like(y_hat)
-    y_low.fill(1.0e-15)
-    return np.maximum(np.minimum(y_hat, y_high), y_low)
-
-
-def log_loss_score(y_true, y_hat):
-    y_hat = stabilize_y_hat(y_hat)
-    out = (y_true*np.log(y_hat) + (1-y_true)*np.log(1-y_hat))
-    return -out.mean(axis=0).mean()
