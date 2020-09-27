1c2f64292eaf8cd684b55cc6df5eefa46d200416
Giovanni d'Ario <giovanni.dario@roche.com>
2020-09-27 15:36:08 +0200

    Make it run on both mac and Linux

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,10 @@
 from pathlib import Path
+import platform
 
-BASE_DIR = Path('/home/giovenko/Projects/moa')
+if platform.system() == "Darwin":
+    BASE_DIR = '/Users/dariog/Projects/moa'
+else:
+    BASE_DIR = '/home/giovenko/Projects/moa'
+
+BASE_DIR = Path(BASE_DIR)
 DATA_DIR = BASE_DIR/'data'
