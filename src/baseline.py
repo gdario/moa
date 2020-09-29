1c41b7fd6cfc6f9b89e4a6bc4835f682c2c9b17c
Giovanni d'Ario <giovanni.dario@roche.com>
2020-09-29 19:32:01 +0200

    Solve the riddle of the multi-label log loss.

--- src/baseline.py
+++ src/baseline.py
@@ -1,48 +1,54 @@
 import numpy as np
-import pandas as pd
-import constants
-import utils
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.model_selection import cross_validate, StratifiedKFold
-from sklearn.metrics import classification_report
-from sklearn.lin
-from xgboost import XGBClassifier
-
+from sklearn.model_selection import KFold
+from sklearn.metrics import log_loss
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import constants
 
 train_ft = pd.read_csv(constants.DATA_DIR/'train_features.csv')
 train_tgt_sc = pd.read_csv(constants.DATA_DIR/'train_targets_scored.csv')
 test_ft = pd.read_csv(constants.DATA_DIR/'test_features.csv')
 
 # There is no point in keeping the vehicle.
-idx_veh_train = train_ft.cp_type == 'ctl_vehicle'
-idx_veh_test = test_ft.cp_type == 'ctl_vehicle'
+idx_trt_train = train_ft.cp_type == 'trt_cp'
+idx_trt_test = test_ft.cp_type == 'trt_cp'
 
-train_x = train_ft[~idx_veh_train]
-train_y = train_tgt_sc[~idx_veh_train]
-test = test_ft[~idx_veh_test]
+train_x = train_ft.loc[idx_trt_train]
+# train_y = train_tgt_sc.loc[idx_trt_train]
+train_y = train_tgt_sc.loc[idx_trt_train].iloc[:, 1:].to_numpy()
+test_x = test_ft.loc[idx_trt_test]
 
-# 1) Drop the id
-# 2) Use OrdinalEncoder on 'cp_type', 'cp_dose'
-column_trans = ColumnTransformer(
-    [('cp_type', OrdinalEncoder(), ['cp_type', 'cp_dose']),
-     ('sig_id', 'drop', 'sig_id')], remainder='passthrough'
+col_trans = ColumnTransformer(
+    [('encode', OrdinalEncoder(), ['cp_type', 'cp_dose']),
+     ('drop_id', 'drop', 'sig_id')],
+    remainder='passthrough'
 )
 
-x = column_trans.fit_transform(train_x)
-y = train_y.iloc[:, 1:train_y.shape[1]].values
+clf = OneVsRestClassifier(LogisticRegression())
+pipe = Pipeline([('preprocess', col_trans), ('clf', clf)])
 
-x_tr, x_val, y_tr, y_val = train_test_split(
-    x, y, test_size=4000, random_state=42)
+n_fold = 0
+train_losses = []
+val_losses = []
 
-# # XGBoostClassifier
-# params = {'max_depth': 2, 'eta': 1, 'objective': 'multi:softprob',
-#           'eval_metric': 'mlogloss', 'num_class': 5, 'subsample': 0.8,
-#           'colsample_bytree': 0.8}
-# clf = OneVsRestClassifier(XGBClassifier(**params))
-# clf.fit(x_tr[:1000], y_tr[:1000])
-# pred_val = clf.predict(x_val)
-# print(classification_report(y_val, pred_val))
+ks = KFold(n_splits=5, shuffle=True, random_state=42)
+
+for idx_train, idx_val in ks.split(train_x, train_y):
+    print("Fold {}".format(n_fold))
+    x_train, y_train = train_x.iloc[idx_train], train_y[idx_train]
+    x_val, y_val = train_x.iloc[idx_val], train_y[idx_val]
+    pipe.fit(x_train, y_train)
+    ypred_train = pipe.predict_proba(x_train)
+    ypred_val = pipe.predict_proba(x_val)
+    loss_train = log_loss(y_train.ravel(), ypred_train.ravel())
+    loss_val = log_loss(y_val.ravel(), ypred_val.ravel())
+    train_losses.append(loss_train)
+    val_losses.append(loss_val)
+    n_fold += 1
+
+# train_losses = np.array(train_losses)
+# val_losses = np.array(val_losses)
